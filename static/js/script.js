e8f01163da7e6a16451dbaa3ad9afcd43e74e31e
DonovanSaez2025 <donovansaez@liceovvh.cl>
2025-08-13 16:03:29 -0400

    anticomentarios
    eliminados los comentarios

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,3 @@
-// Constantes DOM
 const imageBullying1 = document.getElementById("bullying1");
 const imageBullying2 = document.getElementById("bullying2");
 const silentBullying = document.getElementById("silent");
@@ -6,8 +5,6 @@ const boton1 = document.querySelector(".btn1");
 const boton2 = document.querySelector(".btn2");
 const boton3 = document.querySelector(".btn3");
 
-// Acercar o alejar las imágenes o botones al pasar o quitar el mouse de encima
-// Imagen 1
 imageBullying1.addEventListener("mouseover", function () {
   imageBullying1.style.transform = "scale(1.2)";
 });
@@ -15,7 +12,7 @@ imageBullying1.addEventListener("mouseover", function () {
 imageBullying1.addEventListener("mouseout", function () {
   imageBullying1.style.transform = "scale(1)";
 });
-// Imagen 2
+
 imageBullying2.addEventListener("mouseover", function () {
   imageBullying2.style.transform = "scale(1.1)";
 });
@@ -23,7 +20,7 @@ imageBullying2.addEventListener("mouseover", function () {
 imageBullying2.addEventListener("mouseout", function () {
   imageBullying2.style.transform = "scale(1)";
 });
-// Imagen 3
+
 silentBullying.addEventListener("mouseover", function () {
   silentBullying.style.transform = "scale(1.1)";
 });
@@ -32,7 +29,6 @@ silentBullying.addEventListener("mouseout", function () {
   silentBullying.style.transform = "scale(1)";
 });
 
-// Botón 1
 boton1.addEventListener("mouseover", function () {
   boton1.style.transform = "scale(1.2)";
 });
@@ -40,7 +36,7 @@ boton1.addEventListener("mouseover", function () {
 boton1.addEventListener("mouseout", function () {
   boton1.style.transform = "scale(1)";
 });
-// Botón 2
+
 boton2.addEventListener("mouseover", function () {
   boton2.style.transform = "scale(1.1)";
 });
@@ -48,7 +44,7 @@ boton2.addEventListener("mouseover", function () {
 boton2.addEventListener("mouseout", function () {
   boton2.style.transform = "scale(1)";
 });
-// Botón 3
+
 boton3.addEventListener("mouseover", function () {
   boton3.style.transform = "scale(1.1)";
 });
@@ -57,18 +53,14 @@ boton3.addEventListener("mouseout", function () {
   boton3.style.transform = "scale(1)";
 });
 
-// Funciones para cambiar la página
-// ir a acerca del bullying
 function irAcercaDe() {
   window.location.href = ("https://cuidateplus.marca.com/enfermedades/psicologicas/bullying.html");
 }
 
-// Ir a ¿Quiénes Somos?
 function irConsejos() {
   window.location.href = ("https://kidshealth.org/es/teens/bullies.html");
 }
 
-// Ir a ley del bullying
 function irLeyBullying() {
   window.location.href = ("https://www.bcn.cl/leychile/navegar?idNorma=1030087");
 }
